b4e5fa6ee7f15a711d539ee02efd63fbcb19f676
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2024-09-29 21:49:34 -0300

    atualiza e altera o nome questao 1

---
+++ questa 1.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+   int INDICE = 13, SOMA = 0, K = 0;
+    while(K < INDICE){ 
+        K = K + 1; 
+        SOMA = SOMA + K; 
+        
+    }
+    cout<<"Soma: "<<SOMA<<endl;
+
+    return 0;
+}
